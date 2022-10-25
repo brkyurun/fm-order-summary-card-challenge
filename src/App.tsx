c9fb053bb7f1890985db27a05896a781dd2d6ae4
berkay <berkayurun@outlook.com>
2022-10-25 12:52:18 +0300

    init repository

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Attribution } from "./components/Attribution";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl text-pink-600">Hello</h1>
+      <Attribution />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Attribution.tsx
@@ -0,0 +1,20 @@
+export function Attribution() {
+  return (
+    <div className="text-xs text-center absolute bottom-4 text-white">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-purple-300"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="#" className="text-purple-300">
+        Berkay Urun
+      </a>
+      .
+    </div>
+  );
+}
